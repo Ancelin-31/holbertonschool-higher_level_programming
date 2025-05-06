6715796f01160158d280af3275e082195e057398
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-06 20:10:22 +0200

    fix syntax

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,8 +11,8 @@ print(f'Last digit of {number} is {last_digit}')
 if last_digit > 5:
     print('and is greater than 5')
 
-elif last_digit = 0:
+elif last_digit == 0:
     print('and is zero')
 
-elif last_digit < 6 and last_digit /= 0:
+elif last_digit < 6 and last_digit != 0:
     print('and is less than 6 and not zero')
