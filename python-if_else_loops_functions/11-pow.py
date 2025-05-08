0376311cc02f1bffb9d7081ed7a3306a352d52c8
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-08 23:44:00 +0200

    add minus exponent condition

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,5 +2,13 @@
 
 def pow(a, b):
 
-    result = pow(a, b)
+    result = 1
+    if b < 0:
+        for _ in range(-b):
+            result /= a
+
+    else:
+        for _ in range(b):
+            result *= a
+
     return result
