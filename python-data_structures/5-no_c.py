8222c0f347b383afa4c0e5c4da2897cf6d0b5e7c
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-12 14:07:44 +0200

    no more c in string

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+
+    if my_string:
+        new_string = my_string.replace("C", "")
+        final_string = new_string.replace("c", "")
+        return final_string
