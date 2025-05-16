b0149c3ba794a4e1a829d8843d4a14b879005632
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-16 14:19:08 +0200

    adding some tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""
+This module test ....
+"""
+
 
 def add_integer(a, b=98):
-    if not isinstance(a, int) or not isinstance(a, float):
+    """
+    This function.....
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) or not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Let's now learn how to test our own code
