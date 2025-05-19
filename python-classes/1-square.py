dc444d9aa2b7e7dc9225300348c056adad4962eb
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-19 15:12:06 +0200

    add comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
 class Square:
-
+    """Represents a square"""
+    
     def __init__(self, size):
+        """
+        Intializes an new Square.
+        
+        Args:
+            size (int): The size of the square
+        """
         self.__size = size
  
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""Defines a class Square."""
 
 
 class Square:
-    """Represents a square"""
+    """Represents a square."""
 
     def __init__(self, size=0):
         """
         Initializes a new Square.
         
         Args:
-            size (int): The size of the square
-            
+            size (int): The size of the square. Defaults to 0.
+
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
@@ -21,3 +21,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Defines an area method."""
+        return self.__size * self.__size
