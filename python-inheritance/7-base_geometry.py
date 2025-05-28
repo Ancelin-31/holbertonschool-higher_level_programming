3727cea541c7a09dca23e2a670b1051724f3579a
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-28 14:00:27 +0200

    add a valid condition

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,5 +13,7 @@ class BaseGeometry:
         """Checks if value is an integer"""
         if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("<name> must be greater than 0")
+        else:
+            return True
