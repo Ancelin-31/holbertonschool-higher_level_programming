dc45e98f757dc7ee4711b64d1e524d5a17636c2a
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-28 15:14:18 +0200

    correct syntax

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
         """Checks if value is an integer"""
 
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
