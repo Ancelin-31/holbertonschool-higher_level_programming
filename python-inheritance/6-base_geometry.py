66f1e2f4a77a5b07593b4562371e203c6fc465ab
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-28 11:20:35 +0200

    raises an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
