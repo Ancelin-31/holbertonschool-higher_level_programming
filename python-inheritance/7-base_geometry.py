7ac537e57151f6ffd8d1e48e8ad5e202dce3233d
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-28 11:48:48 +0200

    checks if given values are integers

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class BaseGeometry:
+    """Empty class"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if value is an integer"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
