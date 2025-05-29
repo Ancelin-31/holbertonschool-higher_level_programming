7804e8e799b7ba3fc40c6675beb1374543f04ecc
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-05-29 11:37:32 +0200

    defines append, extend, remove, pop methods

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,18 @@
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, iterable):
+        count = len(list(iterable))
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(count))
+
+    def remove(self, item):
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print("Popped [{}] from the list.".format(item))
+        return super().pop(index)
