67c19207b4f1a6b0400b3712934070a9ffb01281
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-02 14:42:52 +0200

    Doesn't jump a line at the end of the output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Defines the reading function"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_file = f.read()
-        print(read_file)
+        print(read_file, end="")
