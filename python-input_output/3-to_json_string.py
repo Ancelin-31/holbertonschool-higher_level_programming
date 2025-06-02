36e87f9d1e8d0265cec6f97cfba2b6d4ebcf187e
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-02 15:41:17 +0200

    returns the json string of a file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Returns the JavaScript Object Notification of a file"""
+
+
+def to_json_string(my_obj):
+    """Defines the JSON returning function"""
+    return json.dumps(my_obj)
