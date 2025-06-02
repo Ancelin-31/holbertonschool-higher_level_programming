79950d4ab0dfa74f6304dab60ea575d1ecffb8c2
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-02 22:22:25 +0200

    creates a class student and classes attributes ready to json conversion

---
+++ python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Creates a class Student"""
+import json
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        if not isinstance(self.first_name, str):
+            raise TypeError('first_name must be a string')
+        if not isinstance(self.last_name, str):
+            raise TypeError('last_name must be a string')
+        if not isinstance(self.age, int):
+            raise TypeError('age must be an int')
+        if self.age <= 0:
+            raise ValueError('age must be more than zero')
+
+    def to_json(self):
+        return self.__dict__
