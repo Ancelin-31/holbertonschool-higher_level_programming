7fa410c473c7dac09dacca6317e25d6ef12d4b57
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-03 18:14:24 +0200

    simplify function

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -11,14 +11,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        if not isinstance(self.first_name, str):
-            raise TypeError('first_name must be a string')
-        if not isinstance(self.last_name, str):
-            raise TypeError('last_name must be a string')
-        if not isinstance(self.age, int):
-            raise TypeError('age must be an int')
-        if self.age <= 0:
-            raise ValueError('age must be more than zero')
 
     def to_json(self):
-        return class_to_json(self)
+        return self.__dict__
