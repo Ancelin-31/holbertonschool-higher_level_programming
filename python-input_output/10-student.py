518a74c0770a896ff4567d2eb3e3a8374c43f25a
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-03 19:11:10 +0200

    classes only given keys of the list

---
+++ python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Creates a class Student"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list) and
+                all(isinstance(attr, str) for attr in attrs)):
+            return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+        else:
+            return self.__dict__
