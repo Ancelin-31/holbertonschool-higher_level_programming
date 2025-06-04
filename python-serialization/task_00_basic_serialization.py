dddb324a996bd2b683501519c68743e71767e15c
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-04 19:03:51 +0200

    adds some comments and simplifies the return

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,10 +4,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
+    """Serializes a dictionary and saves it in a json file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        return f.write(json.dumps(data, f))
+        json.dumps(data, f)
 
 
 def load_and_deserialize(filename):
+    """Loads and deserializes a dictionary from a json file"""
     with open(filename, 'r+', encoding="utf-8") as f:
         return json.load(f)
