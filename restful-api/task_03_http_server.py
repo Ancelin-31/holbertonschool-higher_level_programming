dd1e7fd5e45c52b3887a571041bf03dbe6ab7299
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-11 15:40:18 +0200

    Improve 404 error response message in HTTP server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -25,12 +25,10 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"OK")
         else:
-            self.send_response(404)
+            self.send_response(404, message="Not Found")
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(
-                b"404 Not Found"
-            )
+            self.wfile.write("Endpoint not found")
 
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
