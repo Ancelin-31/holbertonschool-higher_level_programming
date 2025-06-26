e73044b128f5b005bea5fc1df35d08e5c52e6194
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-26 20:45:29 +0200

    Rename table from id_not_null to unique_id in SQL script

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- Create a database assigning a default value to id
-CREATE TABLE IF NOT EXISTS id_not_null (
+CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
