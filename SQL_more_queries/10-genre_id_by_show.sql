4abfe5065407b2d0c241e747208734f155f06f4a
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-27 15:01:42 +0200

    Fix comment and formatting in SQL script to list shows by genre ID

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
--- Lists all shows with at least one genre linked
+-- Lists all shows in hbtn_0d_tvshows ordred by id
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC;
+FROM tv_show_genres
+JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
