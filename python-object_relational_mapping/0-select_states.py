f863e68ef683e5a9bcad4d449f33254d847c8bd5
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-27 19:28:59 +0200

    Refactor database connection in select_states.py and add SQL script to create states table

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa."""
-import MySQLdb
-import sys
+"""List all records of a database"""
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(
+import sys
+import MySQLdb
+
+
+def main():
+    """List a database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=db_name
     )
-    cur = conn.cursor()
+
+    cur = db.cursor()
+
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+
+    for row in cur.fetchall():
         print(row)
+
     cur.close()
-    conn.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
