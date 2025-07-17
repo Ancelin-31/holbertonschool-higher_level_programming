1a2e7dcd54a2b408b938ffb5fa64b2b1c01a0d61
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-07-17 11:11:57 +0200

    Display a list of items

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_file = 'items.json'
+    try:
+        with open(items_file, 'r', encoding='utf-8') as i:
+            data = json.load(i)
+            items_array = data['items']
+    except Exception as e:
+        return ("error : {}".format(e))
+    
+    return render_template('items.html', list=items_array)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
