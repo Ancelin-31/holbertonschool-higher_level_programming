62798d3c1fd680ecad98d9d1d684be6bc9b31aba
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-07-17 16:18:11 +0200

    setup task 3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
+import json
+import csv
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    items_file = 'items.json'
+    try:
+        with open(items_file, 'r', encoding='utf-8') as i:
+            data = json.load(i)
+            items_array = data.get("items")
+        return render_template('items.html', list=items_array)
+
+    except Exception as e:
+        return ("error : {}".format(e))
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+    
+    if source == 'json':
+        data = json.load(data)
+    elif source == 'csv':
+        data = csv.load(data)
+    else:
+        return
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
